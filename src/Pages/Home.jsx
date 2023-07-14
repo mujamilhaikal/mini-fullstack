54e63c994c32b1789413ac1227b149d8992672fd
Muhamad Haikal Mujamil <85897078+mujamilhaikal@users.noreply.github.com>
2023-07-14 21:19:14 +0700

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+import GenreList from '../Components/GenreList';
+import GlobalApi from '../Services/GlobalApi';
+import Banner from '../Components/Banner';
+import TrendingGames from '../Components/TrendingGames';
+import GamesByGenresId from '../Components/GamesByGenresId';
+
+
+function Home() {
+  const [ allGameList, setAllGameList] = useState ();
+  const [ gameListByGenres, setGameListByGenres ] = useState ([]);
+  const [ selectedGenresName, setSelectedGenresName ] = useState ('Action')
+  useEffect(() => {
+    getAllGamesList();
+    getGameListByGenresId(4);
+  },[])
+
+  const getAllGamesList = () => {
+    GlobalApi.getAllGames.then((resp) => {
+      setAllGameList(resp.data.results);
+
+    })
+  }
+
+  const getGameListByGenresId = (id) => {
+    GlobalApi.getGameListByGenreId(id).then((resp) => {
+      console.log("Game List By GenresId", resp.data.results)
+      setGameListByGenres(resp.data.results);
+    })
+  }
+
+  return (
+    <div className='
+    grid
+    grid-cols-4
+    px-8
+    '>
+      <div className='
+        hidden
+        md:block
+      '>
+        <GenreList genreId={(genreId) =>
+        getGameListByGenresId(genreId)}
+        selectedGenresName={(name) => setSelectedGenresName(name)}
+        />
+      </div>
+      <div className='
+        col-span-4
+        md:col-span-3
+      '>
+      {allGameList?.length>0&&gameListByGenres.length>0?
+      <div>
+        <Banner gameBanner = {allGameList[0]}/>
+        <TrendingGames gameList= {allGameList}/>
+        <GamesByGenresId gameList= {gameListByGenres}
+        selectedGenresName={selectedGenresName}
+        />
+      </div>
+        :null}
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Services/GlobalApi.jsx
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const key = import.meta.env.VITE_GAME_API_KEY;
+const axiosCreate = axios.create({
+    baseURL: 'https://api.rawg.io/api'
+})
+
+const getGenreList = axiosCreate.get('/genres?key='+key);
+const getAllGames = axiosCreate.get('/games?key='+key);
+const getGameListByGenreId = (id) => axiosCreate.get('/games?key='+key+'&genres='+id)
+
+export default {
+    getGenreList,
+    getAllGames,
+    getGameListByGenreId
+}
